27601076e9d73a208780f224f409cefed94769bb
Gage Cottrell <inquisitorpinkie@gmail.com>
2022-12-15 12:39:41 -0700

    day 6

--- day6.py
+++ day6.py
@@ -1,3 +1,10 @@
-# day 6
+
 from getinput import fetch_input
 data = fetch_input(2022, 6)
+
+marker_size = 14
+
+for i in range(marker_size, len(data)):
+    if len(set(data[i-marker_size:i])) == marker_size:
+        print(i)
+        break
