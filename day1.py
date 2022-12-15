50e00f00b81d3f277b034bfabb4f508f8a00b0be
Gage Cottrell <inquisitorpinkie@gmail.com>
2022-12-15 12:39:25 -0700

    day 1

--- day1.py
+++ day1.py
@@ -1,3 +1,12 @@
-# day 1
 from getinput import fetch_input
 data = fetch_input(2022, 1)
+elfs = []
+
+for chunk in data.split('\n\n'):
+    elfs.append(sum(map(int, chunk.splitlines())))
+
+print(f'max elf is: {max(elfs)}')
+        
+top_three = list(sorted(elfs))[-3:]
+
+print(f'top three elfs: {sum(top_three)}')
